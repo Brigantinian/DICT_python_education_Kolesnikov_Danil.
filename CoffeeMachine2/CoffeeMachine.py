9181204dc466c45c0cf3436c4cbe14795fc15c51
Kolesnikov <d.v.kolesnikov@student.khai.edu>
2021-10-24 18:29:27 +0300

    2-nd  stage

--- CoffeeMachine2/CoffeeMachine.py
+++ CoffeeMachine2/CoffeeMachine.py
@@ -1,15 +1,15 @@
-def take_coffee(water, milk, coffee_beans, cups):
-    coffee = input('Starting to make a coffee? a - espresso, b - cappuccino, c - latte: ')
-    coffee_espresso_water = 200
-    coffee_espresso_milk = 50
-    coffee_espresso_coffee_beans = 15
-    if coffee == '1':
-        water -= coffee_espresso_water
-        milk -= coffee_espresso_milk
-        coffee_beans -= coffee_espresso_coffee_beans
-        cups -= 1
-        print("Write how many cups of coffee you will need:")
-        print('The coffee machine has:')
-        water = 5000
-        milk = 1250
-        coffee_beans = 375
+def buy_coffee(water, milk, coffee_beans, disposable_cups, money):
+    coffee = input('What do you want to buy? a - espresso, b - latte, c - cappuccino: ')
+
+
+
+
+
+caps = input("Write how many cups of coffee you will need: ")
+water = int(caps) * 200
+milk = int(caps) * 50
+beans = int(caps) * 15
+print("For " + caps + " cups of coffee you will need: ")
+print(str(water) + " ml of water")
+print(str(milk) + " ml of milk")
+print(str(beans) + " g of beans")
