b4bd3ad108dd054ffcadf5b20db6468577bcdb2e
Kolesnikov <d.v.kolesnikov@student.khai.edu>
2021-10-24 15:21:21 +0300

    1-st  stage

---
+++ CoffeeMachine/CoffeeMachine.py
@@ -0,0 +1,49 @@
+#customer_choice = input("espresso,cappuccino,latte")
+class mashine:
+    def init(self):
+     ingredients =["coffe","shugar","milk"]
+
+mashine_on = True
+
+while input("Do you wonna drink something ? (y/n) ").upper() == "y":
+    if print == "y":
+      print("Starting to make a coffee")
+      print("Grinding coffee beans")
+      print("Boiling water")
+      print("Mixing boiled water with crushed coffee beans")
+      print("Pouring coffee into the cup")
+      print("Pouring some milk into the cup")
+      print("Coffee is ready!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
