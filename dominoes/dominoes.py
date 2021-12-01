c9e4ed1420c34758a9b3fd3c00e37aaad1ea4654
Kolesnikov <d.v.kolesnikov@student.khai.edu>
2021-12-01 23:16:21 +0200

    3-rd  stage

--- dominoes/dominoes.py
+++ dominoes/dominoes.py
@@ -1,86 +1,112 @@
-from itertools import combinations
+from itertools import combinations_with_replacement
 from random import sample, choice
 
 
-# random.sample(population, k) - список длиной k из последовательности
-# random.choice(sequence) - случайный элемент непустой последовательности.
-
-def board():
-    domino = list(map(lambda x: list(x), combinations(range(7),
-                                                      2)))  # lambda позволяют функции быть созданной и переданной (зачастую другой функции) в одной строчке кода
+def get_starting_board():
+    domino = list(map(lambda x: list(x), combinations_with_replacement(range(7), 2)))
     while True:
-        player = sample(domino, 7)
-        pc = sample(list(filter(lambda x: x not in player, domino)), 7)
-        stock_pieces = list(filter(lambda x: x not in (player + pc), domino))
-        player_doubles = [bones for bones in player if len(set(bones)) == 1]
-        pc_doubles = [bones for bones in pc if len(set(bones)) == 1]
-        doubles = player_doubles + pc_doubles
+        player_pieces = sample(domino, 7)
+        computer_pieces = sample(list(filter(lambda x: x not in player_pieces, domino)), 7)
+        stock_pieces = list(filter(lambda x: x not in (player_pieces + computer_pieces), domino))
+        player_doubles = [piece for piece in player_pieces if len(set(piece)) == 1]
+        computer_doubles = [piece for piece in computer_pieces if len(set(piece)) == 1]
+        doubles = player_doubles + computer_doubles
         if doubles:
-            max_doubles = max(doubles)
-            if max_doubles in player_doubles:
-                player.remove(max_doubles)
-                first_move = "pc"
+            highest_double = max(doubles)
+            if highest_double in player_doubles:
+                player_pieces.remove(highest_double)
+                first_player="computer"
             else:
-                pc.remove(max_doubles)
-                first_move = "player"
-                return
+                computer_pieces.remove(highest_double)
+                first_player="player"
+            return {'player': player_pieces, 'computer': computer_pieces,
+                    'stock': stock_pieces, 'snake': [highest_double],
+                    'turn': first_player}
 
 
-def board_status():
+def get_board_status():
     if len(board['player']) == 0:
-        return 'The game is over.You win'
-    elif len(board['pc']) == 0:
-        return 'pc win'
-    snake = [num for bones in board['snake'] for num in bones]
+        return 'The game is over. You won!'
+    elif len(board['computer']) == 0:
+        return 'The game is over. The computer won!'
+    flat_snake = [num for piece in board['snake'] for num in piece]  # Flatten the 2-dimensional list into one list
     for i in range(7):
-        for i in board['snake'][0] and i in board['snake'][-1] and snake.count(i) == 8:
-            return "The game is over"
-    return "The game is over 2"
+        if i in board['snake'][0] and i in board['snake'][-1] and flat_snake.count(i) == 8:
+            return "The game is over. It's a draw!"
+    return 'game_not_done'
 
 
-def play_board():
-    print('-' * 70)
-    print('Stock bones:\n', len(board['stock']))
-    print('pc bones\n', len(board['pc']))
+def display_board():
+    print('=' * 70)
+    print('Stock size:', len(board['stock']))
+    print('Computer pieces:', len(board['computer']))
     print()
     snake = board['snake']
     if len(snake) > 6:
         for i in range(3):
             print(snake[i], end='')
-            print(' ', end='')
+        print('...', end='')
         for i in range(-3, 0):
             print(snake[i], end='')
     else:
-        for j in snake:
-            print(j, end='')
+        for p in snake:
+            print(p, end='')
     print()
-    print("your bones:")
-    for i, piece in enumerate(board["player"]):
-        print(f'{i + 1}:{piece}')
+    print('nYour pieces:')
+    for i, piece in enumerate(board['player']):
+        print(f'{i+1}:{piece}')
     print()
 
 
-def game_move(move):
+def is_valid_move(move):
     try:
-       move = int(move)
+        move = int(move)
     except ValueError:
         return False
 
+    return len(board['player']) >= abs(move)
+
 
 def make_move(move, player):
+    """
+    player: can either be "player" or "computer"
+    """
     if move == 0:
         stock_piece = choice(board['stock'])
         board['stock'].remove(stock_piece)
         board[player].append(stock_piece)
         return
-    ind = (move) - 1
-    bones = board[player][ind]
-    board[player].remove(bones)
+    index = abs(move) - 1
+    piece = board[player][index]
+    board[player].remove(piece)
     if move > 0:
-        board['snake'].append(bones)
+        board['snake'].append(piece)
     else:
-        board['snake'].insert(0,bones)
+        board['snake'].insert(0, piece)
 
-board = board()
-while
 
+board = get_starting_board()
+while True:
+    display_board()
+    game_state = get_board_status()
+
+    if game_state != 'game_not_done':
+        print('Status:', game_state)
+        break
+
+    turn = board['turn']
+    if turn == 'player':
+        print("Status: It's your turn to make a move. Enter your command.")
+        while True:
+            move = input()
+            if is_valid_move(move):
+                break
+            print('Invalid input. Please try again.')
+        move = int(move)
+        board['turn'] = 'computer'
+    else:
+        input('Status: Computer is about to make a move. Press Enter to continue...n')
+        move = choice(range(-len(board['computer']), len(board['computer'])))
+        board['turn'] = 'player'
+
+    make_move(move, turn)
