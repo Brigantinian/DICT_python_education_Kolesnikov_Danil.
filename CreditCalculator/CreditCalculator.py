dada6001c521bbcce3c464db1f173195ed2dc465
Kolesnikov <d.v.kolesnikov@student.khai.edu>
2021-12-06 18:55:50 +0200

    git commit -m " protection stage"

--- CreditCalculator/CreditCalculator.py
+++ CreditCalculator/CreditCalculator.py
@@ -12,44 +12,49 @@ arg = parser.parse_args()
 
 type = arg.type
 principal = arg.principal
-interest = arg.interest
+interest1 = arg.interest
 annuity = arg.annuity
 payment = arg.payment
 periods = arg.periods
 
-print(principal, interest, annuity, periods, payment,type)
+print(principal, interest1, annuity, periods, payment,type)
 
-def func1(principal ,periods , interest):
-        p = int(principal)
-        per = int(periods)
-        percent = int(interest)
-        interest = percent / (12 * 100)
-        if percent != 0:
-            interest = float(percent)
-            i = percent / (12 * 100)
-            m = 1
-            D = 0
-            while m <= per:
-                d = (p / per) + (i * (p - (p * (m - 1) / per)))
-                print("Month {}: paid out {}".format(m, math.ceil(d)))
-                m += 1
-                D += math.ceil(d)
-                print("Overpayment = {}".format(D - d))
+def func1():
+    if principal and interest1 and periods:
+            p = int(principal)
+            per = int(periods)
+            percent = int(interest1)
+            interest = percent / (12 * 100)
+            print(interest)
+            if percent != 0:
+                interest = float(percent)
+                print(interest)
+                i = percent / (12 * 100)
+                m = 1
+                D = 0
+                while m <= per:
+                    d = (p / per) + (i * (p - (p * (m - 1) / per)))
+                    print("Month {}: paid out {}".format(m, math.ceil(d)))
+                    m += 1
+                    D += math.ceil(d)
+                    print("Overpayment = {}".format(D - d))
+                else:
+                    print("Incorrect Par3")
             else:
-                print("Incorrect Par3")
-        else:
-            print("Incorrect Par4")
+                print("Incorrect Par4")
 
-
-def func2( principal,  payment , interest,i,x):
+def func2():
+    if principal and payment and interest1:
         p = int(principal)
         pay1 = int(payment)
-        percent = int(interest)
+        percent = int(interest1)
         interest = percent / (12 * 100)
         if percent:
             percent = float(interest)
             i = percent / (12 * 100)
+            print(i)
             x = int(pay1) / (int(pay1) - (interest / 1200 * int(p)))
+            print(x)
             degree = math.log(int(pay1) / (int(pay1) - (interest / 1200 * int(p))),
                               (1 + interest / 1200))
             year = math.ceil(degree) / 12
@@ -58,92 +63,39 @@ def func2( principal,  payment , interest,i,x):
             print("Overpayment =", p * math.ceil(degree) - int(pay1))
         else:
             print("Incorrect Par8")
+    else:
+        print("Incorrect Par9")
 
-
-
-def func3(periods, payment,  interest):
+def func3():
+ if periods and payment and interest1:
         per = int(periods)
         pay2 = int(payment)
-        percent = int(interest)
+        percent = int(interest1)
         interest = percent / (12 * 100)
         if percent == interest:
             percent = float(interest)
             i = percent / (12 * 100)
             num = pow(1 + i, int(per))
             a = i * (1 + i) ** int(per)
+            print(a)
             b = (1 + i) ** int(per) - 1
+            print(b)
             c = i * (1 + i) ** int(per) / (1 + i) ** int(per) - 1
+            print(c)
             d = float(pay2) / i * (1 + i) ** int(per) / (1 + i) ** int(per) - 1
             result_p = math.floor(float(float(payment) / ((i * num) / (num - 1))))
             print("Your loan principal =", round(d), "!")
             print('Overpayment =', float(pay2) * int(per) - result_p)
         else:
-            print("Incorrect Par12")
+            print("Incorrect Par13")
+ else:
+  print("Incorrect Par13")
 
+if __name__=="__main__":
+    func1()
 
+if __name__=="__main__":
+    func2()
 
-
-# if __name__=="__main__":
-#      if principal and periods and interest:
-#         p = int(principal)
-#         per = int(periods)
-#         percent = int(interest)
-#         interest = percent / (12 * 100)
-#         if percent != 0:
-#             interest = float(percent)
-#             i = percent / (12 * 100)
-#             m = 1
-#             D = 0
-#             while m <= per:
-#                 d = (p / per) + (i * (p - (p * (m - 1) / per)))
-#                 print("Month {}: paid out {}".format(m, math.ceil(d)))
-#                 m += 1
-#                 D += math.ceil(d)
-#                 print("Overpayment = {}".format(D - d))
-#             else:
-#                 print("Incorrect Par3")
-#         else:
-#             print("Incorrect Par4")
-#
-# if __name__=="__main__":
-#      if principal and payment and interest:
-#         p = int(principal)
-#         pay1 = int(payment)
-#         percent = int(interest)
-#         interest = percent / (12 * 100)
-#         if percent:
-#             percent = float(interest)
-#             i = percent / (12 * 100)
-#             x = int(pay1) / (int(pay1) - (interest / 1200 * int(p)))
-#             degree = math.log(int(pay1) / (int(pay1) - (interest / 1200 * int(p))),
-#                                       (1 + interest / 1200))
-#             year = math.ceil(degree) / 12
-#             r = str(round(year, (1)))
-#             print("It will take", list(r)[0], "years and", list(r)[2], "months to repay this loan!")
-#             print("Overpayment =", p * math.ceil(degree) - int(pay1))
-#         else:
-#          print("Incorrect Par8")
-#      else:
-#       print("Incorrect Par9")
-#
-# if __name__=="__main__":
-#     if periods and payment and interest:
-#         per = int(periods)
-#         pay2 = int(payment)
-#         percent = int(interest)
-#         interest = percent / (12 * 100)
-#         if percent == interest:
-#             percent = float(interest)
-#             i = percent / (12 * 100)
-#             num = pow(1 + i, int(per))
-#             a = i * (1 + i) ** int(per)
-#             b = (1 + i) ** int(per) - 1
-#             c = i * (1 + i) ** int(per) / (1 + i) ** int(per) - 1
-#             d = float(pay2) / i * (1 + i) ** int(per) / (1 + i) ** int(per) - 1
-#             result_p = math.floor(float(float(payment) / ((i * num) / (num - 1))))
-#             print("Your loan principal =", round(d), "!")
-#             print('Overpayment =', float(pay2) * int(per) - result_p)
-#         else:
-#             print("Incorrect Par12")
-#     else:
-#         print("Incorrect Par13")
+if __name__=="__main__":
+    func3()
