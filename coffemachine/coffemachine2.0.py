89f04b148890e5f3b397bd614b4052682b350526
Kolesnikov <d.v.kolesnikov@student.khai.edu>
2021-12-06 10:31:23 +0200

    coffemachine2.0 3-rd stage

--- coffemachine/coffemachine2.0.py
+++ coffemachine/coffemachine2.0.py
@@ -1,22 +1,3 @@
-print("How many cups you need ?:")
-cups = input()
-print('For ' + str(cups) + ' cups of coffee you will need:')
-water = cups * 200
-milk = cups * 50
-beans = cups * 15
-print(str(water) + ' ml of water')
-print(str(milk) + ' ml of milk')
-print(str(beans) + ' ml of coffee beans')
-
-print("""
-1-Starting to make a coffee
-2-Grinding coffee beans
-3-Boiling water
-4-Mixing boiled water with crushed coffee beans
-5-Pouring coffee into the cup
-6-Pouring some milk into the cup
-7-Coffee is ready!""")
-
 print('Write how many ml of water the coffee machine has:')
 water = int(input())
 print('Write how many ml of milk the coffee machine has:')
@@ -34,3 +15,6 @@ elif cups == Cups:
 else:
     newCups = Cups - cups
     print('Yes, I can make that amount of coffee (and even ' + str(newCups) + ' more than that)')
+
+
+
