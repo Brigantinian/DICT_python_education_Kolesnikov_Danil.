3eb47643943cc60baf8e2bc1146cc8fdce153bae
Kolesnikov <d.v.kolesnikov@student.khai.edu>
2021-12-06 10:29:49 +0200

    coffemachine2.0 1-st stage

---
+++ coffemachine/coffemachine2.0.py
@@ -0,0 +1,8 @@
+print("""
+1-Starting to make a coffee
+2-Grinding coffee beans
+3-Boiling water
+4-Mixing boiled water with crushed coffee beans
+5-Pouring coffee into the cup
+6-Pouring some milk into the cup
+7-Coffee is ready!""")
