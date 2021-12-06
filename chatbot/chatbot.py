a0d77dd05608fa5f0710eb4d2a40ac4ad0f753d9
Kolesnikov <d.v.kolesnikov@student.khai.edu>
2021-12-06 09:31:36 +0200

    chatbot 5-th stage

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -13,3 +13,16 @@ print ("Now i will prove to you that I can count to any number you want.")
 number = int(input())
 for i in range(number + 1):
     print(i, '!', sep='')
+print ("Let's test your programming knowledge.")
+print ("Why do we use methods?")
+print ("1. To repeat a statement multiple times.")
+print ("2. To decompose a program into several small subroutines.")
+print ("3. To determine the execution time of a program.")
+print ("4. To interrupt the execution of a program.")
+x = 0
+while x != 2:
+     x = int(input())
+     if x == 2:
+            print("Completed, have a nice day!")
+     else :
+       print("Please, try again.")
