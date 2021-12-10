2f8b32ced58fc5f5b778b446c916531efa8ff6ae
Kolesnikov <d.v.kolesnikov@student.khai.edu>
2021-12-10 11:06:04 +0200

    r-p-s 3-rd  stage

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -1,15 +1,17 @@
 from random import choice
 
 print("Welcome To Rock-Paper-Scissors")
-player = input("Select any option\nrock\npaper\nscissors ").strip().lower()
-computer = choice("rps")
-if player == computer:
-    print("Draw")
-    print("Computer's choice -> ", computer)
-elif player == "r" and computer == "s" or player == "p" and computer == "r" or player == "s" and computer == "p":
-    print("Player Wins")
-    print("Sorry, but the computer chose another option and failed:", computer)
-else:
-    print("Enter another word")
-    print("Sorry, but the computer chose another option:", computer)
-
+while True:
+    player = input("Select any option\nrock\npaper\nscissors\nexit ").strip().lower()
+    computer = choice("rps")
+    if player == computer:
+        print("Draw")
+        print("Computer's choice -> ", computer)
+    elif player == "r" and computer == "s" or player == "p" and computer == "r" or player == "s" and computer == "p":
+        print("Player Wins")
+        print("Sorry, but the computer chose another option and failed:", computer)
+    else:
+        print("Enter another word")
+        print("Sorry, but the computer chose another option:", computer)
+    if player == "exit":
+     break
