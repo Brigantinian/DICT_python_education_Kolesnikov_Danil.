13f85a8f75a4a7764886a389a7d4b7507ad9460d
Kolesnikov <d.v.kolesnikov@student.khai.edu>
2022-02-09 20:09:11 +0200

    2-nd stage

--- arithmetic_test/arithmetic_test.py
+++ arithmetic_test/arithmetic_test.py
@@ -1,37 +1,62 @@
 import random
 from operator import add, sub, mul
 
+
 score = 0
-while score <= 9:
+number = 0
+
+
+while score <= 5:
     action = (add, sub, mul)
     move = random.choice(action)
-    x = random.randint(2,9)
-    y = random.randint(2,9)
+    x = random.randint(2, 9)
+    y = random.randint(2, 9)
 
     if move == add:
-        print("", x, "add",y, "")
-        example = int(input())
-        answer = move(x,y)
-        if example == answer:
-            print("Right!")
-
-        else:
-            print("Wrong!")
+        try:
+            print("Example: (", x, "+", y, ")")
+            example = int(input())
+            answer = move(x, y)
+            if example == answer:
+                print("Right!")
+                score = score + 1
+                number = number + 1
+            else:
+                print("Wrong!")
+                number = number + 1
+        except:
+            print("Incorrect format")
 
     elif move == sub:
-        print("", x, "sub", y, " ")
-        example = int(input())
-        answer = move(x,y)
-        if example == answer:
-            print("Right!")
-        else:
-            print("Wrong!")
+        try:
+            print("Example: (", x, "-", y, ") ")
+            example = int(input())
+            answer = move(x, y)
+            if example == answer:
+                print("Right!")
+                score = score + 1
+                number = number + 1
+            else:
+                print("Wrong!")
+                number = number + 1
+        except:
+            print("Incorrect format")
 
     elif move == mul:
-        print("", x, "mul", y, " ")
-        example = int(input())
-        answer = move(x,y)
-        if example == answer:
-            print("Right!")
-        else:
-            print("Wrong!")
+        try:
+            print("Example: (", x, "*", y, ") ")
+            example = int(input())
+            answer = move(x, y)
+            if example == answer:
+                print("Right!")
+                score = score + 1
+                number = number + 1
+            else:
+                print("Wrong!")
+                number = number + 1
+        except:
+            print("Incorrect format")
+
+    if number == 5:
+        print("Your score " + str(score) + "/ 5 .")
+        break
