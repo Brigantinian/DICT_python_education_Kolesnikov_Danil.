736c5c90893c554346d1022ae7cb255539028b3b
Kolesnikov <d.v.kolesnikov@student.khai.edu>
2022-05-15 21:24:38 +0300

    2-nd stage

--- currency-exchange/currency-exchange.py
+++ currency-exchange/currency-exchange.py
@@ -1,6 +1,21 @@
+print("аргентинское песо - 1 mycoin = 0.82 ARS;\n"
+        "гондурасская лемпира - 1 mycoin = 0.17 HNL;\n"
+        "австралийский доллар - 1 mycoin = 1.9622 AUD;\n"
+        "марокканский дирхам - 1 mycoin = 0.208 MAD.\n"
+        "американский доллар - 1 mycoin = 3.21 USD")
+
+
 print("Please, enter the number of mycoins you have:")
-a = int(input())
+a = float(input())
 print("Please, enter the exchange rate:")
-b = float(input())
-c = float(a * b)
-print("The total amount of dollars:" + str(c))
+# b = float(input())
+k = float(a * 0.82)
+q = float(a * 0.17)
+s = float(a * 1.9622)
+d = float(a * 0.208)
+f = float(a * 3.21)
+print("I will get "+ str(k) +" ARS from the sale of "+ str(a) +" mycoins.")
+print("I will get "+ str(q) +" HNL from the sale of "+ str(a) +" mycoins.")
+print("I will get "+ str(s) +" AUD from the sale of "+ str(a) +" mycoins.")
+print("I will get "+ str(d) +" MAD from the sale of "+ str(a) +" mycoins.")
+print("I will get "+ str(f) +" USD from the sale of "+ str(a) +" mycoins.")
